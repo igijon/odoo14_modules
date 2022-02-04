4ebde3be892b2f9f4cb9a20e9a1b76407c37c83e
Inma Gijón <igijoninf@gmail.com>
2022-02-04 12:31:36 +0100

    Primer modelo, vista y permisos creado

--- school/models/models.py
+++ school/models/models.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class school(models.Model):
-#     _name = 'school.school'
-#     _description = 'school.school'
-
-#     name = fields.Char()
+class student(models.Model):
+    _name = 'school.student'
+    _description = 'school.student'
+    
+    name = fields.Char()
+    #birth_year = fields.Integer()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
