c6c10ad82f00185f7e965dd1a13fb72810891a23
Inma Gijón <igijoninf@gmail.com>
2022-02-23 19:17:39 +0100

    Clase_23_febrero

--- school/models/models.py
+++ school/models/models.py
@@ -28,12 +28,17 @@ class student(models.Model):
     last_login = fields.Datetime(default=lambda d: fields.Datetime.now())
 
     is_student = fields.Boolean()
+
+    level = fields.Selection([('1','1'),('2','2')])
+
     photo = fields.Image()
     # Este es el formato tradicional para guardar fotos, aunque ahora hay uno
     # imagen en las versiones actuales de Odoo que veremos después
-    classroom = fields.Many2one('school.classroom', ondelete='set null', help='Clase a la que pertecene')
+    classroom = fields.Many2one('school.classroom', domain="[('level','=',level)]", ondelete='set null', help='Clase a la que pertecene')
     teachers = fields.Many2many('school.teacher', related='classroom.teachers', readonly=True)     
 
+    state = fields.Selection([('1','Matriculado'),('2','Estudiante'),('3','Ex-estudiante')], default="1")
+   
     @api.constrains('dni')
     def _check_dni(self):
         regex = re.compile('[0-9]{8}[a-z]\Z', re.I)
@@ -45,11 +50,17 @@ class student(models.Model):
 
     _sql_constraints = [('dni_uniq', 'unique(dni)', 'DNI can\'t be repeated')]
 
+    def regenerate_password(self):
+        for student in self:
+            pw = secrets.token_urlsafe(12)
+            student.write({'password':pw})
+
 class classroom(models.Model):
     _name = 'school.classroom'
     _description = 'Las clases'
     
     name = fields.Char()
+    level = fields.Selection([('1','1'),('2','2')])
     students = fields.One2many(string="Alumnos", comodel_name='school.student', inverse_name='classroom')
     teachers = fields.Many2many(comodel_name='school.teacher', relation='teachers_classroom', column1='classroom_id', column2='teacher_id')
     teachers_last_year = fields.Many2many(comodel_name='school.teacher', relation='teachers_classroom_ly', column1='classroom_id', column2='teacher_id')
@@ -74,5 +85,8 @@ class teacher(models.Model):
     _description = 'Los profesores'
     
     name = fields.Char()
+    topic = fields.Char()
+    phone = fields.Char()
+
     classrooms = fields.Many2many(comodel_name='school.classroom', relation='teachers_classroom', column1='teacher_id', column2='classroom_id')
     classrooms_last_year = fields.Many2many(comodel_name='school.classroom', relation='teachers_classroom_ly', column1='teacher_id', column2='classroom_id')
